24cc68447be653aac7ea584de66681e4fb6e8e78
Jake Drahos <drahos.jake@gmail.com>
2012-11-09 15:20:59 -0600

    Added frame manipulation methods to Sprite

--- rubygame-test.rb
+++ rubygame-test.rb
@@ -12,6 +12,8 @@ class Sprite
   def initialize name, width, frames_number
     super()
 
+    attr_reader :frame_current
+
     @spritesheet = Sprite.load_spritesheet name
     
     @frames = []
@@ -23,6 +25,11 @@ class Sprite
     end
 
     @image = @frames[0]
+    @frame_current = 0
     @rect = @image.make_rect
+  end
+  def frame= index
+    @image = @frames[index]
+    @frame_current = index
   end
 end
